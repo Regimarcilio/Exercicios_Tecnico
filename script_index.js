7318924255a15bfe4f37b875fae44c02c5d1ce87
regimarcilio <reginaldo.marcilio@gmail.com>
2026-06-04 21:06:10 -0300

    nova tela de login

--- script_index.js
+++ script_index.js
@@ -1,5 +1,28 @@
-const btComprar = document.querySelector('#bt-comprar');
-    btComprar.addEventListener('click', () => {
-        alert('Produto adicionado ao carrinho!');
+window.onload = function() {
+    const btComprar = document.querySelectorAll(".comprar");
+    btComprar.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Produto adicionado ao carrinho!');
+        });
     });
+    
+    const melhorCompra1 = document.getElementById("melhorCompra1");
+        melhorCompra1.addEventListener('click', () => { 
+            alert('Essa foi boa! Você ganhou um desconto de 10%!');  
+        });
 
+    const melhorCompra2 = document.getElementById("melhorCompra2");
+        melhorCompra2.addEventListener('click', () => { 
+            alert('Essa foi boa! Você ganhou um desconto de 10%!');
+        });
+
+    const melhorCompra3 = document.getElementById("melhorCompra3");
+        melhorCompra3.addEventListener('click', () => { 
+            alert('Essa foi boa! Você ganhou um desconto de 10%!');
+        }); 
+
+    const melhorCompra4 = document.getElementById("melhorCompra4");
+        melhorCompra4.addEventListener('click', () => { 
+            alert('Essa foi boa! Você ganhou um desconto de 10%!');
+        });
+};
